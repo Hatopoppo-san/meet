ca4bf60462ed8c8ef0a9be4dca766d92d821f462
Hatopoppo-san <hello.melancholinista@gmail.com>
2021-05-14 17:37:58 +0200

    compressed background image

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -11,20 +11,4 @@ describe('<NumberOfEvents /> component', () => {
   test('render text input', () => {
     expect(NumberOfEventsWrapper.find('.number-of-events')).toHaveLength(1);
   });
-
-  // Since eventsPerPage(numberOfEvents) is moved to App state, it needs to be done on integration test on App
-  // test('renders text input correctly', () => {
-  //   const eventPerPage = NumberOfEventsWrapper.props('numberOfEvents');
-  //   expect(NumberOfEventsWrapper.find('.eventsNumber').prop('value')).toBe(32);
-  // });
-
-  // Since eventsPerPage(numberOfEvents) is moved to App state, it needs to be done on integration test on App
-  // test('change state when text input changes', () => {
-  //   NumberOfEventsWrapper.setState({
-  //     eventsPerPage: 5,
-  //   });
-  //   const eventObject = { target: { value: 10 } };
-  //   NumberOfEventsWrapper.find('.eventsNumber').simulate('change', eventObject);
-  //   expect(NumberOfEventsWrapper.state('eventsPerPage')).toBe(10);
-  // });
 });
